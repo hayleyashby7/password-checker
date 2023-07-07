a8bd785e1022240bec9854dee3130eb41ee57546
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-07 15:39:05 +0100

    Progress bar shows red if password is less than 8 characters

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const onLoad = () => {
 	const password = document.getElementById('password');
 	password.addEventListener('input', onInput);
+
+	const progressBar = document.getElementById('progress-bar');
+	progressBar.classList.add('short');
 };
 
 const onInput = (input) => {
 	const progressBar = document.getElementById('progress-bar');
 	progressBar.value = input.target.value.length * 5;
+
+	if (validatePassword(input.target.value)) {
+		progressBar.classList.remove('short');
+	} else {
+		if (!progressBar.classList.contains('short')) progressBar.classList.add('short');
+	}
 };
 
 document.addEventListener('DOMContentLoaded', onLoad);
+
+const validatePassword = (password) => password.length >= 8;
